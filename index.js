39ffc361cf140376b2539f7722c3ef0b89c32676
minahilfatima <sminahilfatima@gmail.com>
2024-03-26 09:53:51 +0500

    created BMI calculator using inquirer

---
+++ index.js
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+let weightInKg = await inquirer.prompt([
+    {
+        name: "weight",
+        type: "number",
+        message: "Enter Your Weight In Kilograms:",
+    },
+]);
+let heightInMeters = await inquirer.prompt([
+    {
+        name: "height",
+        type: "number",
+        message: "Enter Your Height In Meters:",
+    },
+]);
+let BMI = weightInKg.weight / (heightInMeters.height * heightInMeters.height);
+console.log(`Your BMI is ${BMI}`);
